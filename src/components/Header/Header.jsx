1d6e50085d0cccfe76b2a7e0a533399dde5342c6
NafeesButt <nafeesbutt234@gmail.com>
2023-06-15 18:22:53 +0500

    changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,55 +9,50 @@ import { Context } from "../../utils/context";
 import Cart from "../Cart/Cart";
 
 const Header = () => {
-    const [scrolled, setScrolled] = useState(false);
-    const [searchModal, setSearchModal] = useState(false);
-    const navigate = useNavigate();
-    const handleScroll = () => {
-        const offset = window.scrollY;
-        if (offset > 200) {
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-        }
-    };
+  const [scrolled, setScrolled] = useState(false);
+  const [searchModal, setSearchModal] = useState(false);
+  const navigate = useNavigate();
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-    }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
-    const { cartCount, showCart, setShowCart } = useContext(Context);
+  const { cartCount, showCart, setShowCart } = useContext(Context);
 
-    return (
-        <>
-            <header
-                className={`main-header ${scrolled ? "sticky-header" : ""}`}
-            >
-                <div className="header-content">
-                    <ul className="left">
-                        <li onClick={() => navigate("/")}>Home</li>
-                        <li onClick={() => navigate("/about")}>About</li>
-                        <li>Categories</li>
-                    </ul>
-                    <div className="center" onClick={() => navigate("/")}>
-                        JSDEVSTORE.
-                    </div>
-                    <div className="right">
-                        <TbSearch onClick={() => setSearchModal(true)} />
-                        <AiOutlineHeart />
-                        <span
-                            className="cart-icon"
-                            onClick={() => setShowCart(true)}
-                        >
-                            <CgShoppingCart />
-                            {!!cartCount && <span>{cartCount}</span>}
-                        </span>
-                    </div>
-                </div>
-            </header>
-            {searchModal && <Search setSearchModal={setSearchModal} />}
-            {showCart && <Cart />}
-        </>
-    );
+  return (
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li onClick={() => navigate("/")}>Home</li>
+            <li>Categories</li>
+            <li onClick={() => navigate("/about")}>About</li>
+          </ul>
+          <div className="center" onClick={() => navigate("/")}>
+            JSDEVSTORE.
+          </div>
+          <div className="right">
+            <TbSearch onClick={() => setSearchModal(true)} />
+            <AiOutlineHeart />
+            <span className="cart-icon" onClick={() => setShowCart(true)}>
+              <CgShoppingCart />
+              {!!cartCount && <span>{cartCount}</span>}
+            </span>
+          </div>
+        </div>
+      </header>
+      {searchModal && <Search setSearchModal={setSearchModal} />}
+      {showCart && <Cart />}
+    </>
+  );
 };
 
 export default Header;
