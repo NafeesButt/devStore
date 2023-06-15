9f3c705428b3bd1069b7f1bfcacd89947b69e837
NafeesButt <nafeesbutt234@gmail.com>
2023-06-15 18:29:19 +0500

    sales taxt

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -4,25 +4,25 @@ import "./Banner.scss";
 import BannerImg from "../../../assets/banner-img.png";
 
 const Banner = () => {
-    return (
-        <div className="hero-banner">
-            <div className="content">
-                <div className="text-content">
-                    <h1>SALES</h1>
-                    <p>
-                        Convallis interdum purus adipiscing dis parturient
-                        posuere ac a quam a eleifend montes parturient posuere
-                        curae tempor
-                    </p>
-                    <div className="ctas">
-                        <div className="banner-cta">Read More</div>
-                        <div className="banner-cta v2">Shop Now</div>
-                    </div>
-                </div>
-                <img className="banner-img" src={BannerImg} />
-            </div>
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            A sale offers exciting opportunities to save money and obtain
+            desired products or services at discounted prices. Don't miss out on
+            incredible deals and limited-time offers during a sale event!
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
         </div>
-    );
+        <img className="banner-img" src={BannerImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
